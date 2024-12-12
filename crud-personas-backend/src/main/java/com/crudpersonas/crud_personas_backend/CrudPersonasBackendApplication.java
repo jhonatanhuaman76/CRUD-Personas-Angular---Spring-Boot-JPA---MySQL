6faaaea0553a2062ffaa6dd9c7f155654835d55c
Jhonatan <jhonatanhuaman76@gmail.com>
2024-12-12 11:45:32 -0500

    Implementando backend y configurando las propiedades

---
+++ crud-personas-backend/src/main/java/com/crudpersonas/crud_personas_backend/CrudPersonasBackendApplication.java
@@ -0,0 +1,13 @@
+package com.crudpersonas.crud_personas_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudPersonasBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudPersonasBackendApplication.class, args);
+	}
+
+}
---
+++ crud-personas-backend/src/test/java/com/crudpersonas/crud_personas_backend/CrudPersonasBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crudpersonas.crud_personas_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudPersonasBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
