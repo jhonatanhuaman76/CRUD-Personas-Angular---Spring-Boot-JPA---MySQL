6ac502318270e0034e670874b0c4f05a103d169a
Jhonatan <jhonatanhuaman76@gmail.com>
2024-12-12 10:20:01 -0500

    Configurando las rutas del CRUD

--- crud-personas-frontend/src/app/app.routes.ts
+++ crud-personas-frontend/src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { ListarComponent } from './Persona/listar/listar.component';
+import { AddComponent } from './Persona/add/add.component';
+import { EditComponent } from './Persona/edit/edit.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'persona', children: [
+      { path: 'listar', component: ListarComponent },
+      { path: 'add', component: AddComponent },
+      { path: 'edit', component: EditComponent },
+      { path: '', redirectTo: 'listar', pathMatch: 'full' }
+    ]
+  },
+  { path: '', redirectTo: 'persona', pathMatch: 'full' }
+];
