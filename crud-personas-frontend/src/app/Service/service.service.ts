b01639a7456e03adb3f674b0eb9af0aefe9c6113
Jhonatan <jhonatanhuaman76@gmail.com>
2024-12-12 10:49:18 -0500

    Configurando service en el frontend

--- crud-personas-frontend/src/app/Service/service.service.ts
+++ crud-personas-frontend/src/app/Service/service.service.ts
@@ -1,9 +1,16 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Persona } from '../Modelo/Persona';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ServiceService {
-
-  constructor() { }
+  constructor(private http: HttpClient) { }
+  
+  Url: string = 'http://localhost:8080/crud-personas-api/personas';
+  
+  getPersonas() {
+    return this.http.get<Persona[]>(this.Url);
+  }
 }
